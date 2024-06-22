f98a89a3fe2776920d9db3a431b70571cd4bf2af
Yorlin Quispe <yorlinssj@hotmail.com>
2024-06-22 18:29:28 -0500

    update

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ app.get('/form', (req, res) => {
   res.json({ csrfToken: res.locals.csrfToken });
 });
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
