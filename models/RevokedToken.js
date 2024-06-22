0c2804a495fa672db6b8057d512946f94358d5a3
Yorlin Quispe <yorlinssj@hotmail.com>
2024-06-22 17:24:11 -0500

    update reboke

--- models/RevokedToken.js
+++ models/RevokedToken.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const RevokedTokenSchema = new mongoose.Schema({
   token: { type: String, required: true },
-  revokedAt: { type: Date, default: Date.now, expires: '1h' }
+  createdAt: { type: Date, default: Date.now, expires: '1h' } // El token se eliminará automáticamente después de una hora
 });
 
 module.exports = mongoose.model('RevokedToken', RevokedTokenSchema);
